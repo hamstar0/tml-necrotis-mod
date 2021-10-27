802ca15d58b9a6cfbd8fff3ef26e382707ad888b
hamstar0 <webutek@gmail.com>
2021-10-27 03:11:16 -0700

    v2.1.2 - Updated to Pot Luck 2.0 (untested)

--- Necrotis/MyMod_Mods.cs
+++ Necrotis/MyMod_Mods.cs
@@ -1,22 +1,20 @@
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.ModLoader.Config;
 using ModLibsCore.Libraries.Debug;
-using Necrotis.Items;
+using ModLibsGeneral.Libraries.Items;
+using ModLibsGeneral.Libraries.World;
 using PotLuck;
 using CursedBrambles;
+using Necrotis.Items;
 
 
 namespace Necrotis {
 	public partial class NecrotisMod : Mod {
 		private void LoadPotLuckMod() {
-			var myConfig = NecrotisConfig.Instance;
-			var potluckConfig = PotLuckConfig.Instance;
-
-			float ectoDropPerc = myConfig.Get<float>( nameof(myConfig.DillutedEctoplasmPotDropChance) );
-
-			potluckConfig.SetOverride(
+			/*potluckConfig.SetOverride(
 				nameof(PotLuckConfig.PotEntries),
 				new List<PotEntry> {
 					new PotEntry {
@@ -34,7 +32,50 @@ namespace Necrotis {
 						}
 					}
 				}
-			);
+			);*/
+
+			//
+
+			bool OnPotBreak( int tileX, int tileY, out IList<int> droppedItemIndexes ) {
+				droppedItemIndexes = new List<int>();
+
+				if( Main.netMode == NetmodeID.MultiplayerClient ) {
+					return false;
+				}
+
+				//
+
+				var myConfig = NecrotisConfig.Instance;
+				var potluckConfig = PotLuckConfig.Instance;
+
+				float ectoDropPerc = myConfig.Get<float>( nameof( myConfig.DillutedEctoplasmPotDropChance ) );
+
+				if( Main.rand.NextFloat() > ectoDropPerc ) {
+					return false;
+				}
+
+				if( tileY > WorldLocationLibraries.UnderworldLayerTopTileY ) {
+					return false;
+				}
+
+				//
+
+				int itemIdx = ItemLibraries.CreateItem(
+					pos: new Vector2( tileX*16, tileY*16 ),
+					type: ModContent.ItemType<DillutedEctoplasmItem>(),
+					stack: 1,
+					width: 12,
+					height: 12
+				);
+
+				droppedItemIndexes.Add( itemIdx );
+
+				return true;
+			}
+
+			//
+
+			PotLuckAPI.AddPotBreakAction( OnPotBreak );
 		}
 
 
