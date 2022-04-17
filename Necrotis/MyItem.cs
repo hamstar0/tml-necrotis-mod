21b5807c8b3df580af78d15aebff9adc72d925cb
hamstar0 <webutek@gmail.com>
2022-04-17 10:40:50 -0700

    v2.2.3 - Enabled vanilla ectoplasm to be consumable

--- Necrotis/MyItem.cs
+++ Necrotis/MyItem.cs
@@ -10,10 +10,22 @@ namespace Necrotis {
 	class NecrotisGlobalItem : GlobalItem {
 		public override void SetDefaults( Item item ) {
 			if( item.type == ItemID.Ectoplasm ) {
+				item.useStyle = ItemUseStyleID.EatingUsing;
+				item.useAnimation = 15;
+				item.useTime = 15;
+				item.useTurn = true;
+				item.UseSound = SoundID.Item3;
+
 				item.consumable = true;
+				//item.potion = true;
+				//item.healLife = 0;
+				//item.healMana = 0;
+				//item.healMana = 20;
 			}
 		}
 
+
+		////////////////
 
 		public override void ModifyTooltips( Item item, List<TooltipLine> tooltips ) {
 			string modname = "[c/FFFF88:Necrotis] - ";
@@ -41,11 +53,16 @@ namespace Necrotis {
 		}
 
 
-		public override bool ConsumeItem( Item item, Player player ) {
+		////////////////
+
+		public override bool UseItem( Item item, Player player ) {
 			if( item.type == ItemID.Ectoplasm ) {
-				return true;
+				if( player.itemAnimation > 0 && player.itemTime == 0 ) {
+					player.itemTime = item.useTime;
+					return true;
+				}
 			}
-			return base.ConsumeItem( item, player );
+			return base.UseItem( item, player );
 		}
 
 		public override void OnConsumeItem( Item item, Player player ) {
